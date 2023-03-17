24ac1b04c5a8a640fd8f689a378c833a5977d1af
Natalie <nataliembatha@gmail.com>
2023-03-17 09:46:45 +0100

    Compiled changes

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ const dataElement = document.querySelector('#container');
 const button = document.querySelector('#fetch-data-btn');
 button === null || button === void 0 ? void 0 : button.addEventListener('click', getData);
 function generateHTML(apiData) {
-  return apiData.results.map((item) => (`
+    return apiData.results.map((item) => (`
       <div 
         class="w-48 p-2 text-slate-200 text-center bg-slate-700 rounded-lg shadow-md hover:cursor-pointer shadow-black hover:bg-slate-900 hover:translate-y-2 hover:translate-x-2 hover:ease-in duration 300">
         <img src="${item.picture.large}" 
@@ -20,15 +20,15 @@ function generateHTML(apiData) {
       </div>`)).join('');
 }
 function getData() {
-  let htmlStr = ``;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      htmlStr += generateHTML(data);
-      if (dataElement) {
-        dataElement.innerHTML = htmlStr;
-      }
+    let htmlStr = ``;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+        htmlStr += generateHTML(data);
+        if (dataElement) {
+            dataElement.innerHTML = htmlStr;
+        }
     })
-    .catch(err => console.error(err));
+        .catch(err => console.error(err));
 }
 //# sourceMappingURL=index.js.map
